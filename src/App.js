fe9423900b6b94a8d3d88a514d5a320a04c7ff5b
Ivaylo Hristov <ivaylo.hristov@sirma.bg>
2020-02-23 20:17:19 +0200

    Added theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
+import Paperbase from './paperbase/Paperbase'
 
 function App() {
   return (
-    <div className="App">
-     <h1>HI</h1>
-    </div>
+    <Paperbase/>
   );
 }
 
