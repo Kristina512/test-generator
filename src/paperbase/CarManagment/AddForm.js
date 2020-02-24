583be5e0b45315220b6d348975edd39ae2b8f65c
Ivaylo Hristov <ivaylo.hristov@sirma.bg>
2020-02-24 17:55:41 +0200

    save before finish work

--- src/paperbase/CarManagment/AddForm.js
+++ src/paperbase/CarManagment/AddForm.js
@@ -3,7 +3,7 @@ import { TextField, Grid, Button, Box } from "@material-ui/core";
 
 const AddForm = props => {
   return (
-    <div>
+    <div className="addForm" style={styles}>
       <Grid
         container
         spacing={3}
@@ -11,69 +11,78 @@ const AddForm = props => {
         justify="flex-end"
         alignItems="center"
       >
-        <Grid item xs={5}>
+        <Grid container item xs={12} spacing={3} justify="space-evenly">
           <TextField
             id="manufacturer-text-field"
             label="Manufacturer"
             defaultValue=""
             variant="outlined"
+            name="manufacturer"
+            className="textField"
           />
-        </Grid>
-        <Grid item xs={5}>
           <TextField
             id="model-text-field"
             label="Model"
             defaultValue=""
             variant="outlined"
+            name="model"
+            className="textField"
           />
         </Grid>
-        <Grid item xs={5}>
+        <Grid container item xs={12} spacing={3} justify="space-evenly">
           <TextField
             id="fueltype-text-field"
             label="Fuel Type"
             defaultValue=""
             variant="outlined"
+            name="fuelType"
+            className="textField"
           />
-        </Grid>
-        <Grid item xs={5}>
           <TextField
             id="coupetype-text-field"
             label="Coupe Type"
             defaultValue=""
             variant="outlined"
+            name="coupeType"
+            className="textField"
           />
         </Grid>
-        <Grid item xs={5}>
+        <Grid container item xs={12} spacing={3} justify="space-evenly">
           <TextField
             id="horsepower-text-field"
             label="Horse Power"
             type="number"
             defaultValue=""
             variant="outlined"
+            name="horsePower"
+            className="textField"
           />
-        </Grid>
-        <Grid item xs={5}>
           <TextField
             id="gearbox-text-field"
             label="Gear Box Type"
             defaultValue=""
             variant="outlined"
+            name="gearBox"
+            className="textField"
           />
         </Grid>
-        <Grid item xs={5}>
+        <Grid container item xs={12} spacing={3} justify="space-evenly">
           <TextField
             id="color-text-field"
             label="Color"
             defaultValue=""
             variant="outlined"
+            name="color"
+            className="textField"
           />
-        </Grid>
-        <Grid item xs={5}>
           <TextField
             id="seatnumber-text-field"
             label="Number of seats"
+            type="number"
             defaultValue=""
             variant="outlined"
+            name="seatNumber"
+            className="textField"
           />
         </Grid>
       </Grid>
