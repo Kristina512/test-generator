d7ce1056aad0e1f86db3874202f81f6c9217eef9
Ivaylo Hristov <ivaylo.hristov@sirma.bg>
2020-02-24 17:24:56 +0200

    Add from is ready

--- src/paperbase/CarManagment/AddForm.js
+++ src/paperbase/CarManagment/AddForm.js
@@ -1,81 +1,96 @@
 import React from "react";
-import { TextField, Grid } from "@material-ui/core";
+import { TextField, Grid, Button, Box } from "@material-ui/core";
 
 const AddForm = props => {
   return (
-    <Grid
-      container
-      spacing={3}
-      direction="row"
-      justify="flex-end"
-      alignItems="center"
-    >
-      <Grid item xs={5}>
-        <TextField
-          id="manufacturer-text-field"
-          label="Manufacturer"
-          defaultValue=""
-          variant="outlined"
-        />
+    <div>
+      <Grid
+        container
+        spacing={3}
+        direction="row"
+        justify="flex-end"
+        alignItems="center"
+      >
+        <Grid item xs={5}>
+          <TextField
+            id="manufacturer-text-field"
+            label="Manufacturer"
+            defaultValue=""
+            variant="outlined"
+          />
+        </Grid>
+        <Grid item xs={5}>
+          <TextField
+            id="model-text-field"
+            label="Model"
+            defaultValue=""
+            variant="outlined"
+          />
+        </Grid>
+        <Grid item xs={5}>
+          <TextField
+            id="fueltype-text-field"
+            label="Fuel Type"
+            defaultValue=""
+            variant="outlined"
+          />
+        </Grid>
+        <Grid item xs={5}>
+          <TextField
+            id="coupetype-text-field"
+            label="Coupe Type"
+            defaultValue=""
+            variant="outlined"
+          />
+        </Grid>
+        <Grid item xs={5}>
+          <TextField
+            id="horsepower-text-field"
+            label="Horse Power"
+            type="number"
+            defaultValue=""
+            variant="outlined"
+          />
+        </Grid>
+        <Grid item xs={5}>
+          <TextField
+            id="gearbox-text-field"
+            label="Gear Box Type"
+            defaultValue=""
+            variant="outlined"
+          />
+        </Grid>
+        <Grid item xs={5}>
+          <TextField
+            id="color-text-field"
+            label="Color"
+            defaultValue=""
+            variant="outlined"
+          />
+        </Grid>
+        <Grid item xs={5}>
+          <TextField
+            id="seatnumber-text-field"
+            label="Number of seats"
+            defaultValue=""
+            variant="outlined"
+          />
+        </Grid>
       </Grid>
-      <Grid item xs={5}>
-        <TextField
-          id="model-text-field"
-          label="Model"
-          defaultValue=""
-          variant="outlined"
-        />
-      </Grid>
-      <Grid item xs={5}>
-        <TextField
-          id="fueltype-text-field"
-          label="Fuel Type"
-          defaultValue=""
-          variant="outlined"
-        />
-      </Grid>
-      <Grid item xs={5}>
-        <TextField
-          id="coupetype-text-field"
-          label="Coupe Type"
-          defaultValue=""
-          variant="outlined"
-        />
-      </Grid>
-      <Grid item xs={5}>
-        <TextField
-          id="horsepower-text-field"
-          label="Horse Power"
-          type="number"
-          defaultValue=""
-          variant="outlined"
-        />
-      </Grid>
-      <Grid item xs={5}>
-        <TextField
-          id="gearbox-text-field"
-          label="Gear Box Type"
-          defaultValue=""
-          variant="outlined"
-        />
-      </Grid>
-      <Grid item xs={5}>
-        <TextField
-          id="color-text-field"
-          label="Color"
-          defaultValue=""
-          variant="outlined"
-        />
-      </Grid>
-      <Grid item xs={5}>
-        <TextField
-          id="seatnumber-text-field"
-          label="Number of seats"
-          defaultValue=""
-          variant="outlined"
-        />
-      </Grid>
-    </Grid>
+      <Box m={3}>
+        <Button
+          size="large"
+          variant="contained"
+          color="primary"
+          style={{ marginLeft: "15.3%", marginRight: "10px" }}
+        >
+          Save
+        </Button>
+        <Button size="large" variant="contained" color="primary">
+          Clear
+        </Button>
+      </Box>
+    </div>
   );
 };
 
