e60a72fe4bf373bc9dc9cd5560672dfc8a7565a9
ivhristov98@gmail.com <ivaylo.hristov@sirma.bg>
2020-12-14 03:24:37 +0200

    Add option to chose number of questions

--- src/paperbase/TestGenerator/Generator.js
+++ src/paperbase/TestGenerator/Generator.js
@@ -1,9 +1,10 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
 import {withStyles} from "@material-ui/core/styles";
-import {Button, Paper} from "@material-ui/core";
+import {Box, Button, Paper, TextField} from "@material-ui/core";
 import {historyQuestions} from "./tests/History";
 import Question from "./Question";
+import Grid from "@material-ui/core/Grid";
 
 const styles = theme => ({
     paper: {
@@ -31,7 +32,6 @@ const styles = theme => ({
 var shuffle = require('shuffle-array');
 
 
-
 class Generator extends Component {
     constructor(props) {
         super(props);
@@ -41,6 +41,7 @@ class Generator extends Component {
             showQuestions: null
         };
     }
+
     getRandomTest() {
         var shuffled = shuffle(this.state.questions);
         this.setState({questions: shuffled});
@@ -51,23 +52,44 @@ class Generator extends Component {
         this.setState({showQuestions: sliced});
         this.forceUpdate()
     }
+
     render() {
         const {classes} = this.props;
 
         return <div>
             <Paper className={classes.paper}>
                 <div className={classes.contentWrapper}>
-                    <Button variant="contained" color="primary" onClick={() => this.getRandomTest()}>
-                        Generate
-                    </Button>
+                    <Grid container direction="row" alignItems="center" justify="center">
+                        <TextField
+                            onChange={event => {
+                                this.setState({numberOfQuestions: event.target.value});
+                            }}
+                            value={this.state.numberOfQuestions}
+                            id="standard-number"
+                            label="Number of questions"
+                            type="number"
+                            variant="outlined"
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            style={{marginLeft: "2em", marginRight: "2em"}}
+                        />
+                        <Button style={{marginLeft: "2em", marginRight: "2em"}}
+                                variant="contained" color="primary" onClick={() => this.getRandomTest()}>
+                            Generate
+                        </Button>
+                    </Grid>
+
+
                     {
                         this.state.showQuestions != null ? shuffle(this.state.showQuestions.map(e => (
-                            <Question question={e.question} selectedAnswerId={e.selectedAnswerId} answers={e.answers}/>))) : null
+                            <Box my={5} key={e.id}><Question key={e.id} question={e.question} selectedAnswerId={e.selectedAnswerId}
+                                                  answers={e.answers}/></Box>))) : null
                     }
                     {/*TO-DO
-                     1 - Read questions from file
-                     2 - Generate random tests
-                     3 - Add option to chose number of questions
+                     1 - Read questions from file X
+                     2 - Generate random tests X
+                     3 - Add option to chose number of questions X
                      4 - Add option to chose test topic
                      5 - Submit answers and get results
                      */}
--- src/paperbase/TestGenerator/Question.js
+++ src/paperbase/TestGenerator/Question.js
@@ -1,45 +1,32 @@
-import React, {Component} from "react";
-import {FormControl, FormControlLabel, FormLabel, Paper, Radio, RadioGroup} from "@material-ui/core";
-
-// class Question extends Component {
-//     state = {
-//         questionData: {}
-//     };
-//
-//     constructor(props) {
-//         super(props);
-//         this.state = {...props}
-//     }
-//
-//     render() {
-//         return <Paper>
-//             <FormControl component="fieldset">
-//                 <FormLabel component="legend" defaultValue={}>{this.state.questionData.question}</FormLabel>
-//                 <RadioGroup defaultValue={this.state.questionData.answers[this.state.questionData.selectedAnswer - 1]}
-//                             name="customized-radios">
-//                     {this.state.questionData.answers.map(e =>
-//                         <FormControlLabel value={e.value} control={<Radio/>} label={e.value}/>
-//                     )}
-//                 </RadioGroup>
-//             </FormControl>
-//         </Paper>
-//     }
-// }
-//
-// export default Question;
+import React from "react";
+import {
+    Card,
+    CardContent,
+    FormControl,
+    FormControlLabel,
+    FormLabel,
+    Radio,
+    RadioGroup
+} from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
 
 const Question = props => {
-    return (<Paper>
-            <FormControl component="fieldset">
-                <FormLabel component="legend" >{props.question}</FormLabel>
-                <RadioGroup defaultValue={props.answers.find(e => e.id === props.selectedAnswerId).id}
-                            name="customized-radios">
-                    {props.answers.map(e =>
-                        <FormControlLabel value={e.id} control={<Radio/>} label={e.value}/>
-                    )}
-                </RadioGroup>
-            </FormControl>
-        </Paper>
+    return (<Card>
+            <CardContent>
+                <FormControl component="fieldset">
+                    <FormLabel component="legend">
+                    <Typography gutterBottom variant="h5" component="h2">{props.question}</Typography>
+                    </FormLabel>
+                    <RadioGroup defaultValue={props.answers.find(e => e.id === props.selectedAnswerId).id}
+                                name="customized-radios">
+                        {props.answers.map(e =>
+                            <FormControlLabel key={e.id} value={e.id} control={<Radio/>} label={e.value}/>
+                        )}
+                    </RadioGroup>
+                </FormControl>
+            </CardContent>
+
+        </Card>
     )
 }
 
